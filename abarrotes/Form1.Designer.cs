bbce88127c5644362ae1e86cd4d2bf0aeffb8f77
pablo antonio gervacio rodriguez <gervaciopabloantonio@outlook.com>
2022-07-06 16:54:15 -0500

    se esta agregando botones con picturebox para hacer las funcionalidades del cerrar y expandir

--- abarrotes/Form1.Designer.cs
+++ abarrotes/Form1.Designer.cs
@@ -28,44 +28,78 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
             this.panel_principal = new System.Windows.Forms.Panel();
-            this.panelBarra = new System.Windows.Forms.Panel();
+            this.panelMenu = new System.Windows.Forms.Panel();
+            this.panelContenedor = new System.Windows.Forms.Panel();
+            this.BtnCerrar = new System.Windows.Forms.PictureBox();
+            this.panel_principal.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.BtnCerrar)).BeginInit();
             this.SuspendLayout();
             // 
             // panel_principal
             // 
+            this.panel_principal.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(119)))), ((int)(((byte)(141)))), ((int)(((byte)(169)))));
+            this.panel_principal.Controls.Add(this.BtnCerrar);
             this.panel_principal.Dock = System.Windows.Forms.DockStyle.Top;
             this.panel_principal.Location = new System.Drawing.Point(0, 0);
             this.panel_principal.Name = "panel_principal";
-            this.panel_principal.Size = new System.Drawing.Size(1284, 35);
+            this.panel_principal.Size = new System.Drawing.Size(1284, 38);
             this.panel_principal.TabIndex = 1;
             // 
-            // panelBarra
+            // panelMenu
             // 
-            this.panelBarra.Dock = System.Windows.Forms.DockStyle.Left;
-            this.panelBarra.Location = new System.Drawing.Point(0, 35);
-            this.panelBarra.Name = "panelBarra";
-            this.panelBarra.Size = new System.Drawing.Size(200, 576);
-            this.panelBarra.TabIndex = 2;
+            this.panelMenu.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(13)))), ((int)(((byte)(27)))), ((int)(((byte)(42)))));
+            this.panelMenu.Dock = System.Windows.Forms.DockStyle.Left;
+            this.panelMenu.Location = new System.Drawing.Point(0, 38);
+            this.panelMenu.Name = "panelMenu";
+            this.panelMenu.Size = new System.Drawing.Size(146, 573);
+            this.panelMenu.TabIndex = 2;
+            // 
+            // panelContenedor
+            // 
+            this.panelContenedor.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(65)))), ((int)(((byte)(90)))), ((int)(((byte)(119)))));
+            this.panelContenedor.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panelContenedor.Location = new System.Drawing.Point(146, 38);
+            this.panelContenedor.Name = "panelContenedor";
+            this.panelContenedor.Size = new System.Drawing.Size(1138, 573);
+            this.panelContenedor.TabIndex = 3;
+            // 
+            // BtnCerrar
+            // 
+            this.BtnCerrar.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.BtnCerrar.Image = ((System.Drawing.Image)(resources.GetObject("BtnCerrar.Image")));
+            this.BtnCerrar.Location = new System.Drawing.Point(1247, 7);
+            this.BtnCerrar.Name = "BtnCerrar";
+            this.BtnCerrar.Size = new System.Drawing.Size(25, 25);
+            this.BtnCerrar.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.BtnCerrar.TabIndex = 0;
+            this.BtnCerrar.TabStop = false;
+            this.BtnCerrar.Click += new System.EventHandler(this.BtnCerrar_Click);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(1284, 611);
-            this.Controls.Add(this.panelBarra);
+            this.Controls.Add(this.panelContenedor);
+            this.Controls.Add(this.panelMenu);
             this.Controls.Add(this.panel_principal);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
             this.Name = "Form1";
             this.Text = "Form1";
             this.Load += new System.EventHandler(this.Form1_Load);
+            this.panel_principal.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.BtnCerrar)).EndInit();
             this.ResumeLayout(false);
 
         }
 
         #endregion
         private System.Windows.Forms.Panel panel_principal;
-        private System.Windows.Forms.Panel panelBarra;
+        private System.Windows.Forms.Panel panelMenu;
+        private System.Windows.Forms.Panel panelContenedor;
+        private System.Windows.Forms.PictureBox BtnCerrar;
     }
 }
 
--- abarrotes/Form1.cs
+++ abarrotes/Form1.cs
@@ -17,41 +17,18 @@ namespace abarrotes
             InitializeComponent();
         }
 
-        private void ajustesToolStripMenuItem_Click(object sender, EventArgs e)
-        {
-            inventario_ajustes inva = new inventario_ajustes();
-            inva.Show();
-        }
-
-        private void ventasToolStripMenuItem_Click(object sender, EventArgs e)
-        {
-            venta vt = new venta();
-            vt.Show();
-        }
-
-        private void catalogosToolStripMenuItem_Click(object sender, EventArgs e)
-        {
-
-        }
-
-        private void productosToolStripMenuItem_Click(object sender, EventArgs e)
-        {
-            cataproductos ctp = new cataproductos();
-            ctp.Show();
-        }
-
-        private void clientesToolStripMenuItem_Click(object sender, EventArgs e)
-        {
-            cliente ctl = new cliente();
-            ctl.TopLevel = false;
-            panel_principal.Controls.Add(ctl);
-            ctl.Show();
-        }
-
+        
+        
+        
         private void Form1_Load(object sender, EventArgs e)
         {
-            cliente v_cliente = new cliente();
             
+            
+        }
+
+        private void BtnCerrar_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
         }
     }
 }
